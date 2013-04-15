c0c7c883a66b3be2acd4c7097f4b4eb0c9833c47
sarath kumar <sarudsgnr@gmail.com>
2013-04-15 15:42:16 +0530

    first commit
    uploaded the file on git

---
+++ app/controllers/say_controller.rb
@@ -0,0 +1,18 @@
+class SayController < ApplicationController
+  
+def helloo
+ @time = Time.now  end
+
+ 
+
+ def goodbye
+  end
+
+def subpage
+  @day = Time.now.day
+end
+
+def show
+end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'd22fba9e13560a1380b57b0df3a9dcb2a316c280f695deccc6e9f0c4614c6c17fe46018e7acab7a66744fd37b1a9da11bdc5328c6149cb1169b5849377f19e21'
